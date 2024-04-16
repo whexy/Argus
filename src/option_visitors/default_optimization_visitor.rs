dfaf6f53d86303b7ee6fcd31d757b1c215a91ba9
Wenxuan Shi <whexy@outlook.com>
2024-04-15 19:33:03 -0500

    ignore optimization

--- src/option_visitors/default_optimization_visitor.rs
+++ src/option_visitors/default_optimization_visitor.rs
@@ -19,37 +19,17 @@ impl DefaultOptimizationVisitor {
 /// Modify the optimization options. If -Ox is already present, disable it.
 fn modify_optimization_option(options: &mut Vec<CompilerOption>) {
     // disable all options that start with -O
-    let optimization_options = options
-        .iter_mut()
-        .filter(|opt| opt.name.starts_with("-O") && opt.is_enabled)
-        .collect::<Vec<_>>();
-    for opt in optimization_options {
-        opt.disable();
+    if !options
+        .iter()
+        .any(|opt| opt.name.starts_with("-O") && opt.is_enabled)
+    {
+        options.push(CompilerOption::new("-O3"));
+        options.push(CompilerOption::new("-funroll-loops"));
     }
-
-    options.push(CompilerOption::new("-O3"));
-    options.push(CompilerOption::new("-funroll-loops"));
 }
 
 impl OptionVisitor for DefaultOptimizationVisitor {
     fn visit(&mut self, options: &mut Vec<CompilerOption>) {
         modify_optimization_option(options);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::option_manager::CompilerOptionManager;
-
-    use super::*;
-
-    #[test]
-    fn test_visit() {
-        let args = vec![String::from("-O2"), String::from("-g")];
-        let mut manager = CompilerOptionManager::new(args);
-        let mut visitor = DefaultOptimizationVisitor;
-        visitor.visit(&mut manager.options);
-        let expected_options = vec!["-g", "-O3", "-funroll-loops"];
-        assert_eq!(expected_options, manager.get_command());
     }
 }
